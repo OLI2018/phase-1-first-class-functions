ccee0c99210dfbb6942c4255078bcfaa5acaf0c8
Oleg <roxvemm2014@gmail.com>
2021-05-02 13:28:33 -0700

    commit tree 2d35895117457066886d9d570abc5926e0eedd25

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(spy)
+{
+    return spy();
+}
+function returnsANamedFunction()
+{
+return function fn(){}
+}
+function returnsAnAnonymousFunction()
+{
+    return function(){}
+}
+
